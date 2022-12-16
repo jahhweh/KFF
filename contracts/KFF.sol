fbec96eeb79da362f713afb1e5c54ddbad12b60b
jahhweh <zacheidman@gmail.com>
2022-12-16 16:26:04 -0500

    added explainers

--- contracts/KFF.sol
+++ contracts/KFF.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
+// These files are imported from openzeppelin's github repo.
+// When the contract is live, all the functions from these contracts
+// will automatically be imported into our KFF contract
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
@@ -9,43 +12,55 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+// This is the KFF contract.
 contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
     using Strings for uint256;
 
-    string public baseURI;
-    string public baseExtension = ".json";
-    uint256 public cost = 100000000000000000;
-    uint256 public maxSupply = 10001;
-    uint256 public timeDeployed;
-    uint256 public allowMintingAfter;
-    bool public isPaused = false;
-    mapping(address => bool) public philanthropistList;
-    mapping(address => uint256) public philanthropistAmount;
-    address public receiver = 0x311F2A86C44f5040dbaB3D7442670343dFFFECDB;
-    mapping(uint256 => uint256) public hodlStart;
-    mapping(uint256 => uint256) public ranking;
+    // This defines the contracts variables
+    string public baseURI; // this is the URL of the folder containing the metadata
+    string public baseExtension = ".json"; // this is the file extension of the metadata
+    uint256 public cost = 100000000000000000; // this is the cost of minting one NFT, in wei 
+    uint256 public maxSupply = 10001; // this is the maximum supply of NFTs 
+    uint256 public timeDeployed; // this is the time the contract was deployed, in UNIX/EPOCH time
+    uint256 public allowMintingAfter; // this is the time minting is allowed to begin, in UNIX/EPOCH time
+    bool public isPaused = false; // this is the toggle to pause and unpause minting
+    mapping(address => bool) public philanthropistList; // this is a list of wallets that donated eth during mint, known as the philanthropist list
+    mapping(address => uint256) public philanthropistAmount; // this is a list that tracks the total donated eth for wallet addresses, known as the philanthropist amount
+    address public receiver = 0x311F2A86C44f5040dbaB3D7442670343dFFFECDB; // this is the beneficiary wallet
+    mapping(uint256 => uint256) public hodlStart; // this is a list tracking the time a wallet received the NFT, known as hodl time
+    mapping(uint256 => uint256) public ranking; // this is an arbitrary number that can be given to specific NFTs
 
+        // these are the variables required to deploy the contract
     constructor(
-        string memory _name,
-        string memory _symbol,
-        uint256 _allowMintingOn,
-        string memory _initBaseURI
+        string memory _name, // the name 
+        string memory _symbol, // the $TOKEN name
+        uint256 _allowMintingOn, // the unix/epoch date to open minting
+        string memory _initBaseURI // the url of the metadata
         
+        // deploy!
     ) ERC721(_name, _symbol) {
+
+            // set the time minting is allowed to begin
         if (_allowMintingOn > block.timestamp) {
             allowMintingAfter = _allowMintingOn - block.timestamp;
         }
+            // lock in the variables
         cost = cost;
         maxSupply = maxSupply;
         timeDeployed = block.timestamp;
         setBaseURI(_initBaseURI);
     }
 
-        // FANCY MINT FUNCTION
+    // All of the below functions are custom for this contract, 
+    // except for the two functions that are required by openzeppelin. 
+    // NFTs are referred to as Tokens, one NFT is one Token
+
+        // MINT FUNCTION
+    // This will mint one token, start hodl time, set ranking to zero, 
+    // and if applicable, add the wallet and amount donated to philanthropist list and philanthropist amount
     function mint(address _to) public payable {
         uint256 supply = totalSupply();
         require(block.timestamp >= timeDeployed + allowMintingAfter, "Minting is still turned off");
-
         require(supply + 1 <= maxSupply, "Maximum supply has been minted");
         require(!isPaused, "Minting is currently paused");
         require(msg.value >= cost, "Not enough eth");
@@ -65,6 +80,7 @@ contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
     }
 
         // GET A WALLETS OWNED TOKEN IDS
+    // this will search a wallet to check if they own any of this collections NFTs, and which number mint they own
     function getWalletTokenIds(address _owner) public view returns (uint256[] memory) {
         uint256 ownerTokenCount = balanceOf(_owner);
         uint256[] memory tokenIds = new uint256[](ownerTokenCount);
@@ -75,6 +91,7 @@ contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
     }
 
         // GET A TOKENS METADATA LINK
+    // this gets the URL to the metadata of a specific NFT
     function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
         require(_exists(tokenId), "Token number does not exist");
         string memory currentBaseURI = _baseURI();
@@ -83,7 +100,8 @@ contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
                 : "";
     }
 
-        // REQUIRED BY OPENZEPPELIN
+        // REQUIRED BY OPENZEPPELIN FOR PAUSABLE FUNCTIONALITY
+    // this function is required by Openzeppelin for implementing the pause function
     function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
         internal
         whenNotPaused
@@ -92,7 +110,8 @@ contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
         super._beforeTokenTransfer(from, to, tokenId, batchSize);
     }
 
-        // REQUIRED BY OPENZEPPELIN
+        // REQUIRED BY OPENZEPPELIN FOR ENUMBERABLE FUNCTIONALITY
+    // this function is required by Openzeppelin for implementing enumerable functionality
     function supportsInterface(bytes4 interfaceId)
         public
         view
@@ -103,6 +122,10 @@ contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
     }
 
         // OVERRIDE SAFETRANSFER TO SET NEW HODLSTART
+    // this function overrides the safeTransferFrom function
+    // and adds "hodlStart[tokenId] = block.timestamp;"
+    // to reset the hodl time when an NFT is transferred
+    // to a new wallet
     function safeTransferFrom(
         address from,
         address to,
@@ -113,19 +136,23 @@ contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
     }
 
         // GET HODLTIME
+    // this gets the total hodl time for a specific NFT, in seconds
     function getHodlTime(uint256 tokenId) public view returns (uint256) {
         require(_exists(tokenId), "Token number does not exist");
         uint256 _hodlStart = hodlStart[tokenId];
         return block.timestamp - _hodlStart;
     }
 
-        // GET RANKING AND ROLE
+        // GET RANKING
+    // this gets the arbitrary number ranking of a specific NFT
     function getRankingAndRole(uint256 tokenId) public view returns (uint256) {
         require(_exists(tokenId), "Token number does not exist");
         return ranking[tokenId];
     }
 
-        // SET ROLE/TITLE
+        // SET RANKING
+    // this sets the arbitrary number ranking of a specific NFT
+    // only the contract owner can use this function
     function setRole(uint256 tokenId, uint256 _ranking) public payable onlyOwner {
         require(_exists(tokenId), "Token number does not exist");
         require(msg.sender == owner(), "You are not the owner");
@@ -133,36 +160,49 @@ contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
     }
 
         // GET BASE URI
+    // this gets the URL to the NFTs metadata
     function _baseURI() internal view virtual override returns (string memory) {
         return baseURI;
     }
 
         // SET NEW COST FOR MINT
+    // this function will change the cost of minting one NFT
+    // only the contract owner can use this function
     function setCost(uint256 _newCost) public onlyOwner {
         cost = _newCost;
     }
 
         // GET PHILANTHROPIST LIST
+    // this checks if a wallet is on the philanthropy list
+    // and the total amount donated
     function getPhilanthropistList(address _user) public view returns (bool, uint256) {
         return (philanthropistList[_user], philanthropistAmount[_user]);
     }
 
         // CHANGE THE MAIN METADATA HYPERLINK
+    // this changes the URL of the collections metadata
+    // only the contract owner can use this function
     function setBaseURI(string memory _newBaseURI) public onlyOwner {
         baseURI = _newBaseURI;
     }
 
         // CHANGE THE FILE EXTENSION FOR THE MAIN METADATA
+    // this changes the file extension for the URL of the collections metadata
+    // only the contract owner can use this function
     function setBaseExtension(string memory _newBaseExtension) public onlyOwner {
         baseExtension = _newBaseExtension;
     }
 
         // PAUSE AND UNPAUSE MINTING
+    // this pauses and unpauses minting
+    // only the contract owner can use this function
     function setIsPaused(bool _state) public onlyOwner {
         isPaused = _state;
     }
 
         // WITHDRAW FUNDS FROM CONTRACT
+    // this withdraws all the eth in the contract
+    // only the contract owner can use this function
     function withdraw() public payable onlyOwner {
         (bool success, ) = payable(msg.sender).call{
             value: address(this).balance
@@ -171,6 +211,8 @@ contract KFF is ERC721, ERC721Enumerable, Pausable, Ownable, ERC721Burnable {
     }
 
         // BENEFACTOR WITHDRAW FUNDS FROM CONTRACT
+    // this withdraws all the eth in the contract
+    // only the benefactor can use this function
     function receiverWithdraw() public payable {
         require(msg.sender == receiver, "You are not receiver");
         (bool success, ) = payable(msg.sender).call{
